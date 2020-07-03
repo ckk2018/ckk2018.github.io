420ebb356e41a258c01359f80573fba691765da9
ckk2018 <228348443@qq.com>
2020-07-03 11:48:48 +0800

    Initial commit

---
+++ precache-manifest.bad80a6831f1e2df897e2b7c41145ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc54c1153211aa78ac9cb939690324f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c452c5c0e6d9f9e317d",
+    "url": "/static/css/main.ed49ff3f.chunk.css"
+  },
+  {
+    "revision": "01292c53ee168a438fca",
+    "url": "/static/js/2.6687476e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6687476e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c452c5c0e6d9f9e317d",
+    "url": "/static/js/main.f93cb795.chunk.js"
+  },
+  {
+    "revision": "aa932592d05bbe04ab82",
+    "url": "/static/js/runtime-main.06aed258.js"
+  },
+  {
+    "revision": "5f4ff75c5abb15cf11ee7a4140747468",
+    "url": "/static/media/Earth.5f4ff75c.png"
+  }
+]);
